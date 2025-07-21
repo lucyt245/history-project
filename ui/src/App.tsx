ddbbd7fa6d7118c2de278da4614cf6c72e5f3233
Lucy <lmt1g23@soton.ac.uk>
2025-07-21 23:27:11 +0100

    Prints World Timeline in centre of page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,11 @@
+import './App.css';
+
 function App() {
-    return <h1>Hello!</h1>
+    return (
+        <div className="world-title">
+            <h1>World Timeline</h1>
+        </div>
+    )
 }
 
 export default App;
